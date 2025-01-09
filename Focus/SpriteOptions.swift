ecd4871e07c64db9e313cd4e648f60ad478bf0b8
Husein Hakim <76100430+husein-hakim@users.noreply.github.com>
2025-01-09 19:36:57 +0530

    added sharky

---
+++ Focus/SpriteOptions.swift
@@ -0,0 +1,13 @@
+//
+//  SpriteOptions.swift
+//  Focus
+//
+//  Created by Husein Hakim on 09/01/25.
+//
+
+import Foundation
+
+enum SpriteOptions: String, Codable, CaseIterable {
+    case krypton = "krypton"
+    case sharky = "sharky"
+}
--- Focus/SpriteSelectView.swift
+++ Focus/SpriteSelectView.swift
@@ -8,21 +8,38 @@
 import SwiftUI
 
 struct SpriteSelectView: View {
+    @State var sprite: SpriteOptions = .krypton
     var body: some View {
-        ScrollView {
-            ForEach(1...4, id: \.self) {_ in 
-                HStack {
-                    ForEach(1...3, id: \.self) {_ in
-                        ZStack {
-                            
-                            RoundedRectangle(cornerRadius: 10)
-                                .foregroundStyle(Color.black)
-                                .frame(width: 125, height: 125)
-                            
-                            Image("krypton")
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 75, height: 75)
+        ZStack {
+            Color.fSecondary.opacity(0.3).ignoresSafeArea()
+            ScrollView {
+                let spriteCases = SpriteOptions.allCases
+                let rows = Array(stride(from: 0, to: spriteCases.count, by: 3))
+                
+                ForEach(rows, id: \.self) { rowIndex in
+                    HStack {
+                        ForEach(0..<3, id: \.self) { columnIndex in
+                            let index = rowIndex + columnIndex
+                            if index < spriteCases.count {
+                                let currentSprite = spriteCases[index]
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundStyle(sprite == currentSprite ? Color.fPrimary : Color.fSecondary)
+                                        .frame(width: 125, height: 125)
+
+                                    Image(currentSprite.rawValue)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 110, height: 110)
+                                        .clipShape(Circle())
+                                }
+                                .onTapGesture(perform: {
+                                    sprite = currentSprite
+                                })
+                                .padding()
+                            } else {
+                                Spacer()
+                            }
                         }
                     }
                 }
